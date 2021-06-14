aed95e37511bde15150380246d112673f69c14f6
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-06-14 17:29:25 -0600

    Initial setup with router and index

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Switch, useLocation } from "react-router-dom";
+import "./App.css";
+// Main pages
+
+import Home from "./Home";
+
+const FourOhFour = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <h3>
+      No match for <code>{pathname}</code>
+    </h3>
+  );
+};
+function App() {
+  return (
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="*">
+          <FourOhFour />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
