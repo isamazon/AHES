e52262268885262d719e32b6855ee59505ffc0c7
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-06-26 13:45:35 -0600

    changed nav bar layout to responsive nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,44 +1,77 @@
-import React, { useState, Component } from "react";
+import React, { useState } from "react";
+// Dependencies
 import { Link } from "react-router-dom";
-import { AiOutlineMenu } from "react-icons/ai";
+import { BiLogIn } from "react-icons/bi";
+// Components
+
+// CSS
 import "../styles/nav.css";
 
 function Nav() {
   const [click, setClick] = useState(false);
 
+  // Active navbar stat
+  const [nav, setNav] = useState(false);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
+  const changeBackground = () => {
+    if (window.scrollY >= 10) {
+      setNav(true);
+    } else {
+      setNav(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
   return (
-    <div>
-      <nav className="nav-bar">
-        <AiOutlineMenu
-          onClick={handleClick}
-          className="menu-link"
-          size="40px"
-        />
-        <Link to="/" className="homepagelink">
-          AHES
-        </Link>
-      </nav>
-      <div className={click ? "side-menu-container" : "side-menu-closed"}>
-        <h1 onClick={handleClick} className="close-btn">
-          X
-        </h1>
-        <h1 className="menu-h1">Menu</h1>
-        <Link to="/" onClick={closeMobileMenu} className="menu-link-1">
-          Home
-        </Link>
-        <Link to="/Profile" className="menu-link-1 link-mid">
-          Add a book!
-        </Link>
-        <Link className="menu-link-1">Currently reading</Link>
-        <Link to="/" className="menu-link-1 link-mid">
-          My finished books
-        </Link>
-        <Link className="menu-link-1">About</Link>
+    <nav className={nav ? "nav active" : "nav"}>
+      <Link to="/" className="brand">
+        AHES
+      </Link>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={click ? "fas fa-times" : "fas fa-bars"} />
       </div>
-    </div>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">
+          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            Home
+          </Link>
+        </li>
+
+        {/* Dropdown 1 */}
+        <li className="nav-item">
+          <Link
+            to="/Maleshounen"
+            className="nav-links"
+            onClick={closeMobileMenu}
+          >
+            Male Shounen
+          </Link>
+        </li>
+        {/* Dropdown 2 */}
+        <li className="nav-item">
+          <Link
+            to="/Femaleshounen"
+            className="nav-links"
+            onClick={closeMobileMenu}
+          >
+            Female Shounen
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            Contact
+          </Link>
+        </li>
+        <li className="nav-signin">
+          <Link to="/login" className="signinlink">
+            Sign in <BiLogIn className="icon" size="30px" />
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
